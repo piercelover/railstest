5888061633f52540c15baf0732758054f0350e7d
meatwad <brad@hampton.org>
2012-05-04 17:29:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryOne::Application.config.secret_token = 'f122226f2ae840ca674115e2a9f7942b5cfebcbba0e418b838f83f779003a1d1729332a51274351cfffa8b85c8ed7b6ec8d072799fb41a145cc311fe0f3352e4'
